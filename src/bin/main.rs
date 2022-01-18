2752445f5c0ae875aee71380b2a7c48ae420b92b
JPSO <jpso.guerreiro@gmail.com>
2022-01-18 15:29:31 +0000

    implement CLI tool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,12 @@
 use std::env;
 use istor::istor;
+use colored::*;
+
+const AUTHOR: &str = "@Octalbyte";
+const VERSION: &str = "0.1.0";
 
 fn main() {
+    println!("isTor CLI v{} made by {}", VERSION.bold(), AUTHOR.bold());
     let args: Vec<String> = env::args().collect();
+    println!("{}", istor::istor(args[1].as_str()));
 }
