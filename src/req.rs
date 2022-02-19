51c1f6c693a13bf5d6674dadf8c87329ca2cf41e
JPSO <jpso.guerreiro@gmail.com>
2022-02-19 14:26:50 +0000

    ..

--- src/req.rs
+++ src/req.rs
@@ -5,15 +5,12 @@ pub fn find_nodes() {
 
     let reqstr = "
     GET /torbulkexitlist HTTP/2
-    Host: check.torproject.org
-    User-Agent: curl/7.54.0
-    Accept: */*
     ";
     let mut stream = TcpStream::connect("116.202.120.181:80").unwrap();
     stream.write(reqstr.as_bytes()).unwrap();
     let mut resp: String = String::new();
     stream.read_to_string(&mut resp).unwrap();
-    eprintln!("{}", resp);
+    println!("{}", resp);
     //Ok(())
 
 
