aed9f22f6670232885b5a97bb92400c3acac87ec
JPSO <jpso.guerreiro@gmail.com>
2022-02-19 14:06:21 +0000

    reqliv

--- src/req.rs
+++ src/req.rs
@@ -1,9 +1,23 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+
 pub fn find_nodes() {
 
-    //GET /search?q=test HTTP/2
-    //Host: www.bing.com
-    //User-Agent: curl/7.54.0
-    //Accept: */*
+    let reqstr = "
+    GET /torbulkexitlist HTTP/2
+    Host: www.bing.com
+    User-Agent: curl/7.54.0
+    Accept: */*
+    ";
+
+
+
+    let mut stream = TcpStream::connect("check.torproject.org").unwrap();
+    stream.write(reqstr.as_bytes()).unwrap();
+    //stream.read(&mut [0; 128])?;
+    //Ok(())
+
+
 
 
 }
